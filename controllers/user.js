505e8897d3fe890a3caef552c2cb61354e1deca0
Colorado Reed <colorado.j.reed@gmail.com>
2014-06-17 20:41:45 -0700

    switched to mailgun

--- controllers/user.js
+++ controllers/user.js
@@ -277,10 +277,10 @@ exports.postReset = function(req, res, next) {
     },
     function(user, done) {
       var smtpTransport = nodemailer.createTransport('SMTP', {
-        service: 'SendGrid',
+        service: 'Mailgun',
         auth: {
-          user: secrets.sendgrid.user,
-          pass: secrets.sendgrid.password
+          user: secrets.mailgun.user,
+          pass: secrets.mailgun.password
         }
       });
       var mailOptions = {
@@ -355,10 +355,10 @@ exports.postForgot = function(req, res, next) {
     },
     function(token, user, done) {
       var smtpTransport = nodemailer.createTransport('SMTP', {
-        service: 'SendGrid',
+        service: 'Mailgun',
         auth: {
-          user: secrets.sendgrid.user,
-          pass: secrets.sendgrid.password
+          user: secrets.mailgun.user,
+          pass: secrets.mailgun.password
         }
       });
       var mailOptions = {
