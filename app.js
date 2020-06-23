3e3580ea346b4b8b759b881157189bb3e64a7648
Amy Pavel <amypavel@gmail.com>
2020-06-22 21:58:33 -0400

    we shouldn't need https in express - we'll rely on heroku for that

--- app.js
+++ app.js
@@ -191,12 +191,12 @@ MongoClient.connect(secrets.db, function (err, db) {
 
   initServer();
 
-  https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-  }, app).listen(app.get('port'), function() {
+  // https.createServer({
+  //   key: fs.readFileSync('server.key'),
+  //   cert: fs.readFileSync('server.cert')
+  // }, app).listen(app.get('port'), function() {
 
-  // app.listen(app.get('port'), function() {
+  app.listen(app.get('port'), function() {
     console.log(" ✔ Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
   });
 });
